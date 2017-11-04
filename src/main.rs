3dd345cfa7732104b943f4f17801baeab5393614
Jake Howard <git@theorangeone.net>
2017-11-04 13:27:12 +0000

    Write everything

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+const BASE_DIR: &str = "/sys/class/power_supply/BAT0/";
+const DIVISOR: f64 = 1000000000000.0;
+
+
+fn read_file(filename: &str) -> u32 {
+    let filepath = PathBuf::from(BASE_DIR).join(filename);
+    let mut contents = String::new();
+    File::open(filepath).unwrap().read_to_string(&mut contents).expect(&format!("Failed to read {}", filename));
+    return contents.replace("\n", "").parse::<u32>().expect(&format!("Failed to parse {}", filename));
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let voltage = read_file("voltage_now");
+    let current = read_file("current_now");
+    let watts = (voltage as f64 * current as f64) / DIVISOR;
+    println!("{:.2}W", watts);
 }
