67c656af6ee3cb70c0d5e2cb2120f3f5c060b110
Jake Howard <git@theorangeone.net>
2017-11-16 19:02:20 +0000

    Correctly handle errors
    Keep @kierdavis happy!

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ const DIVISOR: f64 = 1000000000000.0;
 
 fn read_file(filename: &str) -> u32 {
     let filepath = PathBuf::from(BASE_DIR).join(filename);
+    let mut file = File::open(filepath).expect(&format!("Could not find file {}", filename));
     let mut contents = String::new();
-    File::open(filepath).unwrap().read_to_string(&mut contents).expect(&format!("Failed to read {}", filename));
+    file.read_to_string(&mut contents).expect(&format!("Failed to read {}", filename));
     return contents.replace("\n", "").parse::<u32>().expect(&format!("Failed to parse {}", filename));
 }
 
